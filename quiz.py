78d9b013476f8944482bd240fa5c765317b0e8dc
Tony <tony.taae@gmail.com>
2019-06-04 11:49:25 -1000

    menu changed

--- quiz.py
+++ quiz.py
@@ -15,7 +15,7 @@
 #  'points' : float
 # }
 
-# quiz type (dict) : 
+# quiz type (dict) :
 # {'id' : questionInfos}
 
 import json
@@ -28,10 +28,10 @@ isModified = False
 def addQuestion(description, text, qtype, answers, feedback, points) :
     global questionId
     global isModified
-    
+
     questionId += 1
     quiz[questionId] = {'description' : description,
-        'text' : text, 
+        'text' : text,
         'qtype' : qtype,
         'answers' : answers,
         'feedback' : feedback,
@@ -41,9 +41,9 @@ def addQuestion(description, text, qtype, answers, feedback, points) :
 
 def modifyQuestion(id, description, text, qtype, answers, feedback, points) :
     global isModified
-    
+
     quiz[id] = {'description' : description,
-        'text' : text, 
+        'text' : text,
         'qtype' : qtype,
         'answers' : answers,
         'feedback' : feedback,
@@ -53,20 +53,20 @@ def modifyQuestion(id, description, text, qtype, answers, feedback, points) :
 
 def deleteQuestion(id) :
     global isModified
-    
+
     del quiz[id]
     isModified = True
-    
+
 def isQuizModified() :
     return isModified
-    
+
 def loadQuizFromFile() :
     try :
         global questionId
         with open(quizFileName) as quizFile :
             tempQuiz = json.load(quizFile)
             quizFile.close()
-            
+
             for qIdStr in tempQuiz.keys() :
                 qIdInt = int(qIdStr)
                 quiz[qIdInt] = tempQuiz[qIdStr]
@@ -78,7 +78,7 @@ def loadQuizFromFile() :
 
 def saveQuizToFile() :
     global isModified
-    
+
     try :
         with open(quizFileName, 'w') as quizFile :
             json.dump(quiz, quizFile)
@@ -92,20 +92,20 @@ def printQuiz():
     for key, value in quiz.items() :
         id=int(key)
         point=quiz[id]['points']
-        if point>1:            
+        if point>1:
             questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' points)'
         else:
-            questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' point)'  
+            questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' point)'
         print(questionTitle)
         print('=' * len(questionTitle), '\n')
         print(value['text'], '\n')
-        
+
         number = 1
         for answer in value['answers'] :
             print("{:d}. {:s}".format(number, answer['text']))
             number += 1
         print('\n')
-       
+
 def isNumber(s):
     try:
         float(s)
@@ -117,37 +117,37 @@ def printQuizMore(numbers):
     numbers=numbers.split(',')
     for key in numbers:
         id=int(key)
-        print('Description: ' + quiz[id]['description'])  
-        print('Type: ' + quiz[id]['qtype'])  
-        print('Question: ' + quiz[id]['text'])     
+        print('Description: ' + quiz[id]['description'])
+        print('Type: ' + quiz[id]['qtype'])
+        print('Question: ' + quiz[id]['text'])
         number = 1
         for answer in quiz[id]['answers'] :
-            if answer['fraction']!=0:       
+            if answer['fraction']!=0:
                 print("Réponse {:d}: {:s} (Fraction:{:d})".format(number, answer['text'], answer['fraction']))
             else:
                 print("Réponse {:d}: {:s}".format(number, answer['text']))
             number += 1
-        print('Feedback:' + quiz[id]['feedback'])  
-        print('Points:' + str(quiz[id]['points']))            
-        print('\n')         
-    
+        print('Feedback:' + quiz[id]['feedback'])
+        print('Points:' + str(quiz[id]['points']))
+        print('\n')
+
 def printQuizPlus(numbers):
     numbers=numbers.split(',')
     for key in numbers:
         id=int(key)
         point=quiz[id]['points']
-        if point>1:            
+        if point>1:
             questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' points)'
         else:
-            questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' point)' 
+            questionTitle = 'Question id ' + str(id) + ' (' + str(point) + ' point)'
         print(questionTitle)
         print('=' * len(questionTitle))
         print(quiz[id]['text'])
         number = 1
         for answer in quiz[id]['answers'] :
-            if answer['fraction']!=0:       
+            if answer['fraction']!=0:
                 print("    {:d}. {:s} ({:d})".format(number, answer['text'], answer['fraction']))
             else:
                 print("    {:d}. {:s}".format(number, answer['text']))
             number += 1
-        print('\n')   
+        print('\n')
